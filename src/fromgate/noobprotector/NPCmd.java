e6a3abbbf34a7e526dd04bc10a26bb1f65cee20c
Igor <Igor@Gate>
2013-07-13 13:46:58 +0400

    v0.0.4

--- src/fromgate/noobprotector/NPCmd.java
+++ src/fromgate/noobprotector/NPCmd.java
@@ -1,16 +1,16 @@
 /*  
  *  NoobProtector, Minecraft bukkit plugin
- *  (c)2012, fromgate, fromgate@gmail.com
+ *  (c)2012-2013, fromgate, fromgate@gmail.com
  *  http://dev.bukkit.org/server-mods/noob-protector/
  *    
  *  This file is part of NoobProtector.
  *  
  *  NoobProtector is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
+ *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  WeatherMan is distributed in the hope that it will be useful,
+ *  NoobProtector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
@@ -30,123 +30,125 @@ import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 
 public class NPCmd implements CommandExecutor{
-	NoobProtector plg;
-	NPUtil u;
-	
-
-	public NPCmd (NoobProtector plg){
-		this.plg = plg;
-		this.u = plg.u;
-
-	}
+    NoobProtector plg;
+    NPUtil u;
 
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
-		if (sender instanceof Player){
-			Player p = (Player) sender;
+    public NPCmd (NoobProtector plg){
+        this.plg = plg;
+        this.u = plg.u;
 
-			if (cmdLabel.equalsIgnoreCase("noob")){
-				if ((args.length>0)&&u.CheckCmdPerm(p, args[0])){
-					if (args.length==1) return ExecuteCmd (p, args[0]);
-					else if (args.length==2) return ExecuteCmd (p, args[0],args[1]);
-					else if (args.length==3) return ExecuteCmd (p, args[0],args[1],args[2]);
-				} else u.PrintPxMSG(p, "cmd_cmdpermerr",'c');
-				return true;
-			} else if (cmdLabel.equalsIgnoreCase("pvp-on")){
-				if (!p.hasPermission("noob-protector.pvp-on")) return true;
-				if (plg.players.getPvpOff(p))	{
-					if (checkPvpOnCooldown(p)){
-						plg.players.unprotectPlayer(p);
-						u.PrintMSG(p, "msg_warnpvpon",'6');						
-					} else {
-						u.PrintMSG (p,"msg_currenttime",u.getServerTime(plg.timezone),'3','9');
-						plg.players.printPlayerProtected(p, true,false);
-						u.PrintMSG(p, "msg_pvponcooldown","/pvp-on;"+plg.pvponcooldown);
-					}
-				} else u.PrintMSG(p, "msg_alreadyunprotected",'c');
-				
-				return true;
-			}
-		} 
-		return false;
-	}
-	
-	private boolean checkPvpOnCooldown(Player p){
-		Long ct = System.currentTimeMillis();
-		
-		if ((!p.hasMetadata("NP-pvp-on-cooldown"))||
-				((ct-p.getMetadata("NP-pvp-on-cooldown").get(0).asLong())>(plg.pvponcooldown*1000))){
-			p.setMetadata("NP-pvp-on-cooldown", new FixedMetadataValue (plg, ct));
-			return false;
-		}
-		return true;
-	}
+    }
 
 
-	private boolean ExecuteCmd(Player p, String cmd) {
-		if (cmd.equalsIgnoreCase("help")){
-			u.PrintHLP(p);
-			u.PrintMSG(p, "msg_pvponcmd","/pvp-on");
-		} else if (cmd.equalsIgnoreCase("protect")){
-			plg.players.setPlayer(p);
-			plg.players.printPlayerProtected(p, false,true);
-		} else if (cmd.equalsIgnoreCase("unprotect")){
-			if (plg.players.unprotectPlayer(p)) u.PrintMSG(p, "msg_youunprotected");
-			else u.PrintMSG(p, "msg_unprtfail",p.getName());
-		} else if (cmd.equalsIgnoreCase("list")){
-			plg.players.printList(p,1,"");
-		} else if (cmd.equalsIgnoreCase("reload")){
-			plg.reloadConfig();
-			plg.loadCfg();
-			u.PrintMSG(p, "msg_reloaded");
-		} else if (cmd.equalsIgnoreCase("cfg")){
-			u.PrintCfg(p);
-			
-		} else return false;
-		return true;
-	}
-	
-	private boolean ExecuteCmd(Player p, String cmd, String arg) {
-		if (cmd.equalsIgnoreCase("help")){
-			u.PrintHLP(p, arg);
-		} else if (cmd.equalsIgnoreCase("protect")){
-			Player prp = Bukkit.getPlayerExact(arg);
-			if ((prp!=null)&&(prp.isOnline())){
-				plg.players.setPlayer(prp);
-				plg.players.printPlayerProtected(prp, false,true);
-				plg.players.printTargetPlayerProtected(p, prp);
-			} else u.PrintMSG (p, "msg_unknownplayer",arg);
-		} else if (cmd.equalsIgnoreCase("unprotect")){
-			Player prp = Bukkit.getPlayerExact(arg);
-			if (plg.players.unprotectPlayer(arg)){
-				if ((prp!=null)&&(prp.isOnline())) u.PrintMSG(prp, "msg_youunprotected");
-				u.PrintMSG(p, "msg_plrisunprotected",arg);	
-			} else u.PrintMSG(p, "msg_unprtfail",arg);
-		} else if (cmd.equalsIgnoreCase("list")){
-			int pnum = 1;
-			String mask = "";
-			if (arg.matches("[1-9]+[0-9]*")) pnum = Integer.parseInt(arg);
-			else mask = arg;
-			plg.players.printList(p,pnum,mask);
-		} else return false;		 
-		return true;
-	}
-	
-	private boolean ExecuteCmd(Player p, String cmd, String arg1,String arg2) {
-		if (cmd.equalsIgnoreCase("list")){
-			int pnum = 1;
-			String mask = "";
-			if (arg1.matches("[1-9]+[0-9]*")) {
-				pnum = Integer.parseInt(arg1);
-				mask = arg2;
-			} else if (arg2.matches("[1-9]+[0-9]*")) {
-				pnum = Integer.parseInt(arg2);
-				mask = arg1;				
-			}
-			plg.players.printList(p,pnum,mask);
-		} else return false;
-		return true;
-	}
+    @Override
+    public boolean onCommand(CommandSender p, Command cmd, String cmdLabel, String[] args) {
+        if (cmdLabel.equalsIgnoreCase("noob")){
+            if ((args.length>0)&&u.checkCmdPerm(p, args[0])){
+                if (args.length==1) return ExecuteCmd (p, args[0]);
+                else if (args.length==2) return ExecuteCmd (p, args[0],args[1]);
+                else if (args.length==3) return ExecuteCmd (p, args[0],args[1],args[2]);
+            } else u.printMSG(p, "cmd_cmdpermerr",'c');
+            return true;
+        } else if (cmdLabel.equalsIgnoreCase("pvp-on")){
+            if (!(p instanceof Player)) return false;
+            Player player = (Player) p;
+            if (!player.hasPermission("noob-protector.pvp-on")) return true;
+            if (plg.players.getPvpOff(player))	{
+                if (checkPvpOnCooldown(player)){
+                    plg.players.unprotectPlayer(player);
+                    u.printMSG(p, "msg_warnpvpon",'6');						
+                } else {
+                    u.printMSG (p,"msg_currenttime",'3','9',u.getServerTime(plg.timezone));
+                    plg.players.printPlayerProtected(player, true,false);
+                    u.printMSG(p, "msg_pvponcooldown","/pvp-on;"+plg.pvponcooldown);
+                }
+            } else u.printMSG(p, "msg_alreadyunprotected",'c');
+
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkPvpOnCooldown(Player p){
+        Long ct = System.currentTimeMillis();
+        if ((!p.hasMetadata("NP-pvp-on-cooldown"))||
+                ((ct-p.getMetadata("NP-pvp-on-cooldown").get(0).asLong())>(plg.pvponcooldown*1000))){
+            p.setMetadata("NP-pvp-on-cooldown", new FixedMetadataValue (plg, ct));
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean ExecuteCmd(CommandSender p, String cmd) {
+        if (cmd.equalsIgnoreCase("help")){
+            u.PrintHlpList(p, 1, 15);
+            u.printMSG(p, "msg_pvponcmd","/pvp-on");
+        } else if (cmd.equalsIgnoreCase("protect")){
+            if (!(p instanceof Player)) return false;
+            Player player = (Player) p;
+            plg.players.setPlayer(player);
+            plg.players.printPlayerProtected(player, false,true);
+        } else if (cmd.equalsIgnoreCase("unprotect")){
+            if (!(p instanceof Player)) return false;
+            Player player = (Player) p;
+            if (plg.players.unprotectPlayer(player)) u.printMSG(p, "msg_youunprotected");
+            else u.printMSG(p, "msg_unprtfail",p.getName());
+        } else if (cmd.equalsIgnoreCase("list")){
+            plg.players.printList(p,1,"");
+        } else if (cmd.equalsIgnoreCase("reload")){
+            plg.reloadConfig();
+            plg.loadCfg();
+            u.printMSG(p, "msg_reloaded");
+        } else if (cmd.equalsIgnoreCase("cfg")){
+            u.PrintCfg(p);
+        } else return false;
+        return true;
+    }
+
+    private boolean ExecuteCmd(CommandSender p, String cmd, String arg) {
+        if (cmd.equalsIgnoreCase("help")){
+            int page = 1;
+            if (u.isInteger(arg)) page = Integer.parseInt(arg);
+            u.PrintHlpList(p, page, 15);
+        } else if (cmd.equalsIgnoreCase("protect")){
+            Player prp = Bukkit.getPlayerExact(arg);
+            if ((prp!=null)&&(prp.isOnline())){
+                plg.players.setPlayer(prp);
+                plg.players.printPlayerProtected(prp, false,true);
+                plg.players.printTargetPlayerProtected(p, prp);
+            } else u.printMSG (p, "msg_unknownplayer",arg);
+        } else if (cmd.equalsIgnoreCase("unprotect")){
+            Player prp = Bukkit.getPlayerExact(arg);
+            if (plg.players.unprotectPlayer(arg)){
+                if ((prp!=null)&&(prp.isOnline())) u.printMSG(prp, "msg_youunprotected");
+                u.printMSG(p, "msg_plrisunprotected",arg);	
+            } else u.printMSG(p, "msg_unprtfail",arg);
+        } else if (cmd.equalsIgnoreCase("list")){
+            int pnum = 1;
+            String mask = "";
+            if (arg.matches("[1-9]+[0-9]*")) pnum = Integer.parseInt(arg);
+            else mask = arg;
+            plg.players.printList(p,pnum,mask);
+        } else return false;		 
+        return true;
+    }
+
+    private boolean ExecuteCmd(CommandSender p, String cmd, String arg1,String arg2) {
+        if (cmd.equalsIgnoreCase("list")){
+            int pnum = 1;
+            String mask = "";
+            if (arg1.matches("[1-9]+[0-9]*")) {
+                pnum = Integer.parseInt(arg1);
+                mask = arg2;
+            } else if (arg2.matches("[1-9]+[0-9]*")) {
+                pnum = Integer.parseInt(arg2);
+                mask = arg1;				
+            }
+            plg.players.printList(p,pnum,mask);
+        } else return false;
+        return true;
+    }
 
 }
--- src/fromgate/noobprotector/NPUtil.java
+++ src/fromgate/noobprotector/NPUtil.java
@@ -1,6 +1,6 @@
 /*  
  *  NoobProtector, Minecraft bukkit plugin
- *  (c)2012, fromgate, fromgate@gmail.com
+ *  (c)2012-2013, fromgate, fromgate@gmail.com
  *  http://dev.bukkit.org/server-mods/noob-protector/
  *    
  *  This file is part of NoobProtector.
@@ -10,7 +10,7 @@
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  WeatherMan is distributed in the hope that it will be useful,
+ *  NoobProtector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
@@ -21,153 +21,131 @@
  */
 
 package fromgate.noobprotector;
-/*  
- *  (c)2012, fromgate, fromgate@gmail.com
- *  http://dev.bukkit.org/server-mods/weatherman/
- *   * 
- *  This file is part of ReActions.
- *  
- *  WeatherMan is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  WeatherMan is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with WeatherMan.  If not, see <http://www.gnorg/licenses/>.
- * 
- */
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
-
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 
 
 public class NPUtil extends FGUtilCore {
-	NoobProtector plg;
+    NoobProtector plg;
 
-	public NPUtil(NoobProtector plugin, boolean vcheck, boolean savelng, String language, String devbukkitname, String version_name, String plgcmd, String px){
-		super (plugin, vcheck, savelng, language, devbukkitname, version_name, plgcmd, px);
-		this.plg = plugin;
+    public NPUtil(NoobProtector plugin, boolean vcheck, boolean savelng, String language, String devbukkitname, String version_name, String plgcmd, String px){
+        super (plugin, vcheck, savelng, language, devbukkitname, version_name, plgcmd, px);
+        this.plg = plugin;
 
-		FillMSG();
-		InitCmd();
-
-
-		if (savelng) this.SaveMSG();
-	}
-
-	public void PrintCfg(Player p){
-		PrintMsg(p, "&6&lNoobProtector v"+des.getVersion()+" &r&6| "+MSG("cfg_configuration",'6'));
-		PrintMSG (p,"msg_currenttime",getServerTime(""));
-		PrintMSG (p,"msg_servertimezone",TimeZone.getDefault().getDisplayName());
-		PrintMSG (p, "msg_cfgtimezone", plg.timezone);
-		PrintMSG (p,"msg_currenttime",getServerTime(plg.timezone));
-		PrintEnDis (p, "msg_joinprotect", plg.joinprotect);
-		PrintEnDis (p, "msg_playerwarn", plg.playerwarn);
-		PrintMSG (p, "msg_playerwarntime", plg.playerwarntime);
-		PrintEnDis (p, "msg_useplaytime", plg.useplaytime);
-		PrintMSG (p, "msg_useplaytimevalue", plg.prtplay);
-		PrintEnDis (p, "msg_userealtime", plg.userealtime);
-		PrintMSG (p, "msg_userealtimevalue", plg.prttime);
-		PrintMSG (p, "msg_pvponcooldowntime", plg.pvponcooldown);
-		PrintMSG (p, "msg_pvpupdatetime", plg.pvpupdatetime);
-		PrintMSG (p, "msg_language", plg.language);
-		PrintEnDis (p, "msg_versioncheck", plg.version_check);
-
-	}
+        FillMSG();
+        InitCmd();
 
 
+        if (savelng) this.SaveMSG();
+    }
 
-	public void InitCmd(){
-		cmds.clear();
-		cmdlist = "";
-		addCmd("help", "config",MSG("cmd_help","&3/noob help [command]",'b'),true);
-		addCmd("protect", "protect",MSG("cmd_protect","&3/noob protect [player]",'b'));
-		addCmd("unprotect", "unprotect",MSG("cmd_unprotect","&3/noob unprotect [player]",'b'));
-		addCmd("list", "config",MSG("cmd_list","&3/noob list [page] [name mask]",'b'),true);
-		addCmd("reload", "config",MSG("cmd_reload","&3/noob reload",'b'),true);
-		addCmd("cfg", "config",MSG("cmd_cfg","&3/noob cfg",'b'),true);
-	}
-
-	public void FillMSG(){
-		//NPPList
-		addMSG ("msg_warnpvpon", "Warning! PVP-protection removed. You can now attack and be attacked by other players!"); 
-		addMSG ("msg_plisttitle", "Noob-protected players");
-		addMSG ("msg_plistfooter", "Page: [%1% / %2%]");
-		
-		
-		addMSG ("msg_emptylist", "There's no Noob-protected players");
-
-		//cmd 
-		addMSG ("cmd_protect", "%1% - protect player (or protect yourself)");
-		addMSG ("cmd_protect", "%1% - unprotect player (or unprotect yourself)");
-		addMSG ("cmd_list", "%1% - list all protected players");
-		addMSG ("cmd_reload", "%1% - reload configuration from file");
-		addMSG ("cmd_cfg", "%1% - show plugin configuration");
-		
-		addMSG ("msg_reloaded", "configuration reloaded from file");
-		
-		addMSG ("msg_pvponcmd", "%1% - to disable your own protection");
-		addMSG ("msg_youunprotected", "Your PVP-protection was removed");
-		addMSG ("msg_unprtfail", "Failed to remove %1%'s protection");	
-		
-		addMSG ("msg_plrisprotected", "%1% is now protected");
-		addMSG ("msg_plrisunprotected", "%1% is unprotected");
-		
-		addMSG ("msg_unknownplayer", "Player %1% is unknown. May be he is offline?");		
-		addMSG ("msg_unprtfail", "Failed to remove protection from %1%");	
-
-		addMSG ("msg_alreadyunprotected", "You are already unprotected!");
-		addMSG ("msg_pvponcooldown", "Type %1% again during the %2% seconds to unprotect yourself");
-		
-		addMSG ("msg_currenttime", "Current (server) time is %1%");
+    public void PrintCfg(CommandSender p){
+        printMsg(p, "&6&lNoobProtector v"+des.getVersion()+" &r&6| "+getMSG("cfg_configuration",'6'));
+        printMSG (p,"msg_currenttime",getServerTime(""));
+        printMSG (p,"msg_servertimezone",TimeZone.getDefault().getDisplayName());
+        printMSG (p, "msg_cfgtimezone", plg.timezone);
+        printMSG (p,"msg_currenttime",getServerTime(plg.timezone));
+        printEnDis (p, "msg_joinprotect", plg.joinprotect);
+        printEnDis (p, "msg_playerwarn", plg.playerwarn);
+        printMSG (p, "msg_playerwarntime", plg.playerwarntime);
+        printEnDis (p, "msg_useplaytime", plg.useplaytime);
+        printMSG (p, "msg_useplaytimevalue", plg.prtplay);
+        printEnDis (p, "msg_userealtime", plg.userealtime);
+        printMSG (p, "msg_userealtimevalue", plg.prttime);
+        printMSG (p, "msg_pvponcooldowntime", plg.pvponcooldown);
+        printMSG (p, "msg_pvpupdatetime", plg.pvpupdatetime);
+        printMSG (p, "msg_language", plg.language);
+        printEnDis (p, "msg_versioncheck", plg.version_check);
+    }
 
 
-		//Listener
-		addMSG ("msg_warnpvpoff", "You are now protected from PVP-attacks. But you cannot attack other players too. PVP-protection will be removed at %1%");
-		addMSG ("msg_youcantattack", "Hey! You cannot attack other players! Type %1% to remove protection.");
-		addMSG ("msg_warndefender", "%1% is trying to attack you!");
-		addMSG ("msg_warnatacker", "%1% is PVP-protected. Your attack failed.");		
 
-		addMSG ("msg_protected", "You are protected.");
-		addMSG ("msg_notprotected", "You are unprotected.");
-		
-		addMSG ("msg_playtime", "Play-time (online) limit: %1%");
-		addMSG ("msg_realtime", "Unprotection time: %1%");
-		addMSG ("msg_typepvpon", "Type %1% to remove protection.");
-		
-		
-		addMSG ("cfg_configuration", "Configuration");
-		
-		addMSG ("msg_servertimezone", "Default (system) time zone: %1%");
-		addMSG ("msg_cfgtimezone", "Time zone modifier (defined in config-file): %1%");
-		addMSG ("msg_currenttime", "Time in defined time zone is %1%");
-		
-		addMSG ("msg_joinprotect", "Auto protect new players");
-		addMSG ("msg_playerwarn", "Warn player about protection");
-		addMSG ("msg_playerwarntime", "Warning message delay: %1% minutes");
-		addMSG ("msg_useplaytime", "Use play-time (online) protection");
-		addMSG ("msg_useplaytimevalue", "Play-time protection limit %1% minutes");
-		addMSG ("msg_userealtime", "Use real-time protection");
-		addMSG ("msg_userealtimevalue", "Real-time protection limit %1% minutes");
-		addMSG ("msg_pvponcooldowntime", "/pvp-on command delay: %1% seconds");
-		addMSG ("msg_pvpupdatetime", "Update protection status every %1% minutes");
-		addMSG ("msg_language", "Language: %1%");
-		addMSG ("msg_versioncheck", "Check plugin updates");
-	}
-	
-	public String getServerTime (String tzone){
-		Date d = new Date (System.currentTimeMillis());
-		SimpleDateFormat f =  new SimpleDateFormat ("dd/MM/yyyy HH:mm");
-		if (!tzone.isEmpty()) f.setTimeZone(TimeZone.getTimeZone(tzone));
-		return f.format(d);
-	}
+    public void InitCmd(){
+        cmds.clear();
+        cmdlist = "";
+        addCmd("help", "config","hlp_thishelp","&3/noob help [command]",'b',true);
+        addCmd("protect", "protect","cmd_protect","&3/noob protect [player]",'b',true);
+        addCmd("unprotect", "unprotect","cmd_unprotect","&3/noob unprotect [player]",'b',true);
+        addCmd("list", "config","cmd_list","&3/noob list [page] [name mask]",'b',true);
+        addCmd("reload", "config","cmd_reload","&3/noob reload",'b',true);
+        addCmd("cfg", "config","cmd_cfg","&3/noob cfg",'b',true);
+    }
+
+    public void FillMSG(){
+        //NPPList
+        addMSG ("msg_warnpvpon", "Warning! PVP-protection removed. You can now attack and be attacked by other players!"); 
+        addMSG ("msg_plisttitle", "Noob-protected players");
+        addMSG ("msg_plistfooter", "Page: [%1% / %2%]");
+
+
+        addMSG ("msg_emptylist", "There's no Noob-protected players");
+
+        //cmd 
+        addMSG ("cmd_protect", "%1% - protect player (or protect yourself)");
+        addMSG ("cmd_unprotect", "%1% - unprotect player (or unprotect yourself)");
+        addMSG ("cmd_list", "%1% - list all protected players");
+        addMSG ("cmd_reload", "%1% - reload configuration from file");
+        addMSG ("cmd_cfg", "%1% - show plugin configuration");
+
+        addMSG ("msg_reloaded", "configuration reloaded from file");
+
+        addMSG ("msg_pvponcmd", "%1% - to disable your own protection");
+        addMSG ("msg_youunprotected", "Your PVP-protection was removed");
+        addMSG ("msg_unprtfail", "Failed to remove %1%'s protection");	
+
+        addMSG ("msg_plrisprotected", "%1% is now protected");
+        addMSG ("msg_plrisunprotected", "%1% is unprotected");
+
+        addMSG ("msg_unknownplayer", "Player %1% is unknown. May be he is offline?");		
+        addMSG ("msg_unprtfail", "Failed to remove protection from %1%");	
+
+        addMSG ("msg_alreadyunprotected", "You are already unprotected!");
+        addMSG ("msg_pvponcooldown", "Type %1% again during the %2% seconds to unprotect yourself");
+
+        addMSG ("msg_currenttime", "Current (server) time is %1%");
+
+
+        //Listener
+        addMSG ("msg_warnpvpoff", "You are now protected from PVP-attacks. But you cannot attack other players too. PVP-protection will be removed at %1%");
+        addMSG ("msg_youcantattack", "Hey! You cannot attack other players! Type %1% to remove protection.");
+        addMSG ("msg_warndefender", "%1% is trying to attack you!");
+        addMSG ("msg_warnatacker", "%1% is PVP-protected. Your attack failed.");		
+
+        addMSG ("msg_protected", "You are protected.");
+        addMSG ("msg_notprotected", "You are unprotected.");
+
+        addMSG ("msg_playtime", "Play-time (online) limit: %1%");
+        addMSG ("msg_realtime", "Unprotection time: %1%");
+        addMSG ("msg_typepvpon", "Type %1% to remove protection.");
+
+
+        addMSG ("cfg_configuration", "Configuration");
+
+        addMSG ("msg_servertimezone", "Default (system) time zone: %1%");
+        addMSG ("msg_cfgtimezone", "Time zone modifier (defined in config-file): %1%");
+        addMSG ("msg_currenttime", "Time in defined time zone is %1%");
+
+        addMSG ("msg_joinprotect", "Auto protect new players");
+        addMSG ("msg_playerwarn", "Warn player about protection");
+        addMSG ("msg_playerwarntime", "Warning message delay: %1% minutes");
+        addMSG ("msg_useplaytime", "Use play-time (online) protection");
+        addMSG ("msg_useplaytimevalue", "Play-time protection limit %1% minutes");
+        addMSG ("msg_userealtime", "Use real-time protection");
+        addMSG ("msg_userealtimevalue", "Real-time protection limit %1% minutes");
+        addMSG ("msg_pvponcooldowntime", "/pvp-on command delay: %1% seconds");
+        addMSG ("msg_pvpupdatetime", "Update protection status every %1% minutes");
+        addMSG ("msg_language", "Language: %1%");
+        addMSG ("msg_versioncheck", "Check plugin updates");
+    }
+
+    public String getServerTime (String tzone){
+        Date d = new Date (System.currentTimeMillis());
+        SimpleDateFormat f =  new SimpleDateFormat ("dd/MM/yyyy HH:mm");
+        if (!tzone.isEmpty()) f.setTimeZone(TimeZone.getTimeZone(tzone));
+        return f.format(d);
+    }
 
 }
